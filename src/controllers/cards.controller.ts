3aa5870541185c72a2f12ca5e09c4d127ca719bf
ursula <tom@michaels-MacBook-Pro.local>
2022-11-08 12:00:41 +0000

    set up model

--- src/controllers/cards.controller.ts
+++ src/controllers/cards.controller.ts
@@ -1,24 +1,22 @@
 import * as path from 'path';
 const fs = require("fs/promises");
+const { selectCards } = require('../models/cards.model')
 
 console.log("in the controller")
 
 exports.getCards = (request, response) => {
-    
     try {
-            return fs.readFile(path.resolve(__dirname, `../data/cards.json`))
-                .then((cards) => {
-                  
-                  if (cards.length > 0) {
-                    response.status(200).send(  cards )
-                 }
-                  else {
-                    response.status(404).send("Items not found")
-                  }
-                })
-                
-              } catch (error) {
-                
-              }   
+        selectCards()
+          .then((cards) => {   
+            if (cards.length > 0) {
+              response.status(200).send(  cards )
+              }
+              else {
+              response.status(404).send("Items not found")
+              }
+            })
+          } catch (error) {
+          
+          }   
         }       
 
--- src/models/cards.model.ts
+++ src/models/cards.model.ts
@@ -2,3 +2,16 @@ import * as path from 'path';
 const fs = require("fs/promises");
 
 console.log("in the model")
+
+exports.selectCards = () => {
+    try {
+        return fs.readFile(path.resolve(__dirname, `../data/cards.json`))
+        .then((cardsData) => {
+            const cards = JSON.parse(cardsData);
+            return cards
+        })
+
+    } catch (error) {
+
+    }
+}

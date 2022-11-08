06ae0c5afb6cb4c9de5e812faf8f9931e85c9d75
ursula <tom@michaels-MacBook-Pro.local>
2022-11-08 12:27:25 +0000

    set up cardsList

--- src/models/cards.model.ts
+++ src/models/cards.model.ts
@@ -5,13 +5,19 @@ console.log("in the model")
 
 exports.selectCards = () => {
     try {
+        const cardList: object[] = [];
         return fs.readFile(path.resolve(__dirname, `../data/cards.json`))
         .then((cardsData) => {
             const cards = JSON.parse(cardsData);
-            return cards
-        })
-
+            //return cards
+            
+            cardList.push({
+                "title": cards[0]["title"],               
+            })
+            console.log(cardList);
+            return cardList;
+        }) 
     } catch (error) {
 
     }
-}
+};

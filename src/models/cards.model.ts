f54b8d3c4d78b4aef765447cb39e92eabcac255b
ursula <tom@michaels-MacBook-Pro.local>
2022-11-11 10:58:30 +0000

    model - remove unnecessary if statment

--- src/models/cards.model.ts
+++ src/models/cards.model.ts
@@ -27,14 +27,14 @@ exports.selectCards = async () => {
 exports.selectCard = async ( cardId ) => {
     try {
         const singleCardList: object[] = [];
+
         const cardsData = await fs.readFile(path.resolve(__dirname, `../data/cards.json`));
         const cards = JSON.parse(cardsData);
         const foundCard = cards.find((card: {id: string}) => card.id === cardId)
+        
         const templatesData = await fs.readFile(path.resolve(__dirname, `../data/templates.json`))
         const templates = JSON.parse(templatesData);
-
-        if (foundCard) {
-            let foundTemplate = templates.find((template: {id: string}) => template.id === foundCard["pages"][0]["templateId"])
+        const foundTemplate = templates.find((template: {id: string}) => template.id === foundCard["pages"][0]["templateId"])
             
             singleCardList.push({
                 "title": foundCard["title"],
@@ -44,8 +44,7 @@ exports.selectCard = async ( cardId ) => {
                 "availableSizes": foundCard["sizes"],
                 "pages": foundCard["pages"]
             })
-        }
-        return singleCardList[0];
+            return singleCardList[0];     
     } catch (error) {
         return error;   
     }  
